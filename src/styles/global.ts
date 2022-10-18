076df44dc2ceb984736db3dff707d3ea32a390c6
Yasmin Gonçalves <yasmin.goncalves97@hotmail.com>
2022-10-17 21:59:58 -0300

    Initial commit

---
+++ src/styles/global.ts
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    * {
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+    }
+
+    body {
+        height: 100vh;
+        background-image: ${props => props.theme.linear};
+        background-repeat: no-repeat;
+
+        color: ${props => props.theme.text};
+    }
+
+    body, input, textarea, button {
+        font: 400 1rem 'Questrial', sans-serif;
+    }
+`
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,11 @@
+export const defaultTheme = {
+    'blue-100': '#3B5170',
+    'blue-200': '#314561',
+
+    'pink': '#C69793',
+
+    'text': '#F7FFF6',
+    'placeholder': '#3B5170',
+
+    'linear': 'linear-gradient(359.45deg, #C69894 -0.35%, #314561 99.53%)'
+} as const
